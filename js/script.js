58e924540b746b1cd26999a6424be94cccff0572
brownbear2212 <74186542+greggpbrown@users.noreply.github.com>
2024-02-12 23:04:53 +0000

    more pages and responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 // My custom JavaScript goes here
 
-let date = new Date();
-let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-let formattedDate = new Intl.DateTimeFormat('en-GB', options).format(date);
+// Find the nav bar
+const navbar = document.querySelector('nav');
+const currentPage = "./" + window.location.href.split('/').pop();
 
-document.querySelector('.time').innerHTML = formattedDate;
+// Using fetch API to read the JSON file
+fetch('../data/sitemap.json')
+    .then(response => response.json())
+    .then(data => {
+        // Loop through the data
+        data.forEach(item => {
+            // Create a new element for each item in the data
+            let element = document.createElement('div');
+
+            // Add classes to the element
+            element.classList.add('nav-item');
+
+            // Check if current page
+            if (currentPage == item.pageURL) {
+                element.classList.add('current');
+            }
+            // Add the icon
+            let icon = document.createElement('i');
+            icon.classList.add('fa', item.navIcon);
+            element.appendChild(icon);
+
+            // Create an anchor tag for the page link
+            let link = document.createElement('a');
+            link.href = item.pageURL;
+            link.textContent = item.pageTitle;
+
+            // Append the link to the element
+            element.appendChild(link);
+
+            // Append the element to the document body or other container
+            navbar.appendChild(element);
+        });
+    })
+    .catch(error => console.error('Error:', error));
+
+console.log(currentPage)
