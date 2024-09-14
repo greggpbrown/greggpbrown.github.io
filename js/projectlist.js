8c3efa9ac714994e7dc509c6e648d94e3e3fd366
Gregg Brown <74186542+greggpbrown@users.noreply.github.com>
2024-09-14 19:26:39 +0100

    rollback

--- js/projectlist.js
+++ js/projectlist.js
@@ -12,23 +12,22 @@ fetch('../data/projects.json')
             // Create an article element
             let article = document.createElement('article');
 
-            // Add the URL
-            let pageURL = document.createElement('a');
-            pageURL.href = item.pageURL;
-            // pageURL.textContent = "View "+ item.pageTitle +" Project";
-            pageURL.target = "_blank";
-            article.appendChild(pageURL);
-
             // Add the title
             let title = document.createElement('h3');
             title.textContent = item.pageTitle;
-            pageURL.appendChild(title);
+            article.appendChild(title);
 
             // Add the description
             let description = document.createElement('p');
             description.textContent = item.description;
-            pageURL.appendChild(description);
+            article.appendChild(description);
 
+            // Add the URL
+            let pageURL = document.createElement('a');
+            pageURL.href = item.pageURL;
+            pageURL.textContent = "View "+ item.pageTitle +" Project";
+            pageURL.target = "_blank";
+            article.appendChild(pageURL);
 
             // Append to project list
             projectlist.appendChild(article);            
